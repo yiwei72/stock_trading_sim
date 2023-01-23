80766d40a96957c909c5408ce6af74b8a5cace7c
Cuiyang Wang <c222wang@uwaterloo.ca>
2023-01-23 11:53:49 -0500

    finish MyBatis connection and database tests

--- backend/src/main/java/com/company/project/controllers/HomeController.java
+++ backend/src/main/java/com/company/project/controllers/HomeController.java
@@ -1,17 +1,19 @@
 package com.company.project.controllers;
 
+import com.company.project.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/*
 @RestController
 public class HomeController {
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/")
-    public Greeting showHome(String name, Model model) {
-        return repository.findById(1).orElse(new Greeting("Not Found 😕"));
+    public void showHome(String name, Model model) {
     }
 
 }
-*/
--- backend/src/test/java/com/company/project/DatabaseTests.java
+++ backend/src/test/java/com/company/project/DatabaseTests.java
@@ -1,22 +1,23 @@
 package com.company.project;
 
+import com.company.project.mapper.UserMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.sql.DataSource;
-import java.sql.SQLException;
 
 @SpringBootTest
 class DatabaseTests {
 
     @Autowired
     DataSource dataSource;
+    @Autowired
+    private UserMapper userMapper;
 
     @Test
-    void testConnection() throws SQLException {
-        System.out.println(dataSource.getClass());
-        System.out.println(dataSource.getConnection());
+    void testConnection() {
+        System.out.println(userMapper.selectByExample(null).toString());
     }
 
 }
