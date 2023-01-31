c3b002581fcf8011377f35fbaf8044bce513053b
Evelyn-xqy <53362789+Evelyn-xqy@users.noreply.github.com>
2023-01-31 08:47:03 -0500

    Add login&signup API to connect to backend (#14)
    * Install axios
    
    * Add signup API to connect to backend
    
    * Add login API to connect to backend

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,33 +1,80 @@
 import React, { useState } from "react";
+import axios from 'axios';
 
 interface LoginProps {
   handleLogin: () => void;
   handleSignUp: () => void;
 }
 
+interface LoginUserData {
+  email: string;
+  password: string;
+}
+
 const Login: React.FC<LoginProps> = ({ handleLogin, handleSignUp }) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [loginUserData, setUserData] = useState<LoginUserData>({
+    email: '',
+    password: '',
+  });
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setUserData({ ...loginUserData, [name]: value });
+  };
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setIsLoading(true);
+
+    try {
+      const response = await axios.post('/api/admin/login', loginUserData);
+      console.log(response.data);
+      if (response.data.resultCode == 200) {
+        handleLogin();
+      } else {
+        setErrorMessage(response.data.data);
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
     <div className="login-container" style={{ display: "grid", placeItems: "center", columnGap: 20 }}>
       <h1>Login</h1>
-      <input
-        type="text"
-        placeholder="Username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <div style={{ display: "grid", placeItems: "center", columnGap: 20 }}>
-        <button onClick={handleLogin}>Log In</button>
-        <button onClick={handleSignUp}>Don't have an account? Sign up here.</button>
-      </div>
+      <form onSubmit={handleSubmit}>
+        <div style={{ display: "grid", placeItems: "center", columnGap: 20 }}>
+          <input
+            type="text"
+            placeholder="Email"
+            name="email"
+            value={loginUserData.email}
+            onChange={handleInputChange}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={loginUserData.password}
+            onChange={handleInputChange}
+          />
+
+        </div>
+        <div style={{ display: "grid", placeItems: "center", columnGap: 20 }}>
+          {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+          <button type="submit" disabled={isLoading}>
+            {isLoading ? 'Logging In...' : 'Log In'}
+          </button>
+          <button onClick={handleSignUp}>Don't have an account? Sign up here.</button>
+        </div>
+      </form>
     </div>
   );
 };
--- frontend/src/components/Signup.tsx
+++ frontend/src/components/Signup.tsx
@@ -1,8 +1,16 @@
 import React, { useState }  from "react";
+import axios from 'axios';
 
 interface SignupProps {
   handleLogin: () => void;
   handleLogout: () => void;
+}
+
+interface SignupUserData {
+  email: string;
+  password: string;
+  firstName: string;
+  lastName: string;
 }
 
 const Signup: React.FC<SignupProps> = ({ handleLogin, handleLogout }) => {
@@ -11,44 +19,88 @@ const Signup: React.FC<SignupProps> = ({ handleLogin, handleLogout }) => {
   const [confirm_password, setConfirmPassword] = useState("");
   const [first_name, setFirstName] = useState("");
   const [last_name, setLastName] = useState("");
+  const [signupUserData, setUserData] = useState<SignupUserData>({
+    email: '',
+    password: '',
+    firstName: '',
+    lastName: '',
+  });
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setUserData({ ...signupUserData, [name]: value });
+  };
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setIsLoading(true);
+
+    try {
+      const response = await axios.post('/api/admin/signup', signupUserData);
+      console.log(response.data);
+      if (response.data.resultCode == 200) {
+        handleLogin();
+      } else {
+        setErrorMessage(response.data.data);
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
     <div style={{ display: "grid", placeItems: "center", columnGap: 20 }}>
       <h1>Sign up</h1>
-      <input
-        type="text"
-        placeholder="Username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Confirm Password"
-        value={password}
-        onChange={(e) => setConfirmPassword(e.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="Your First Name"
-        value={password}
-        onChange={(e) => setFirstName(e.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="Your Last Name"
-        value={password}
-        onChange={(e) => setLastName(e.target.value)}
-      />
-      <div style={{ display: "grid", placeItems: "center", columnGap: 20 }}>
-        <button onClick={handleLogout}>Already have an account? Log in here.</button>
-        <button onClick={handleLogin}>Confirm Sign up</button>
-      </div>
+      <form onSubmit={handleSubmit}>
+        <div style={{ display: "grid", placeItems: "center", columnGap: 20 }}>
+          <input
+            type="text"
+            placeholder="Email"
+            name="email"
+            value={signupUserData.email}
+            onChange={handleInputChange}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={signupUserData.password}
+            onChange={handleInputChange}
+          />
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            value={confirm_password}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Your First Name"
+            name="firstName"
+            value={signupUserData.firstName}
+            onChange={handleInputChange}
+          />
+          <input
+            type="text"
+            placeholder="Your Last Name"
+            name="lastName"
+            value={signupUserData.lastName}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div style={{ display: "grid", placeItems: "center", columnGap: 20 }}>
+          {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+          <button onClick={handleLogout}>Already have an account? Log in here.</button>
+          <button type="submit" disabled={isLoading}>
+            {isLoading ? 'Signing Up...' : 'Sign Up'}
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
