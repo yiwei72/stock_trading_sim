6aca30d8fa0691ee561a34e96310edb9b3ee34fd
Yiwei Wang <yiwei.wang@momenta.ai>
2023-02-05 18:01:17 -0500

    add test cases to login

--- frontend/src/tests/Login.test.tsx
+++ frontend/src/tests/Login.test.tsx
@@ -1,5 +1,6 @@
+import axios from 'axios';
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, waitFor } from '@testing-library/react';
 import Login from '../components/Login';
 
 jest.mock('axios', () => ({
@@ -7,35 +8,64 @@ jest.mock('axios', () => ({
   post: jest.fn(() => Promise.resolve({ data: {} })),
 }));
 
+describe('Test Login component', () => {
+  it('email and password input boxes can be filled correctly', () => {
+    const handleLogin = jest.fn();
+    const handleSignUp = jest.fn();
+    const { getByPlaceholderText } = render(<Login handleLogin={handleLogin} handleSignUp={handleSignUp} />);
+  
+    const emailInput = getByPlaceholderText('Email');
+    fireEvent.change(emailInput, { target: { value: 'test@example.com', name: 'email' } });
+    expect(emailInput.getAttribute('value')).toBe('test@example.com');
+  
+    const passwordInput = getByPlaceholderText('Password');
+    fireEvent.change(passwordInput, { target: { value: 'password', name: 'password' } });
+    expect(passwordInput.getAttribute('value')).toBe('password');
+  });
+  
+  it('displays an error message when email or password is missing', () => {
+    const handleLogin = jest.fn();
+    const handleSignUp = jest.fn();
+    const { getByPlaceholderText, getByText } = render(<Login handleLogin={handleLogin} handleSignUp={handleSignUp} />);
+  
+    const emailInput = getByPlaceholderText('Email');
+    fireEvent.change(emailInput, { target: { value: '', name: 'email' } });
+  
+    const passwordInput = getByPlaceholderText('Password');
+    fireEvent.change(passwordInput, { target: { value: '', name: 'password' } });
+  
+    const submitButton = getByText('Log In');
+    fireEvent.click(submitButton);
+  
+    const errorMessage = getByText('All fields are required');
+    expect(errorMessage).toBeDefined();
+  });
 
-it('The email and password input boxes can be filled correctly', () => {
-  const handleLogin = jest.fn();
-  const handleSignUp = jest.fn();
-  const { getByPlaceholderText } = render(<Login handleLogin={handleLogin} handleSignUp={handleSignUp} />);
+  it('should make a POST request to the correct endpoint', async () => {
+    // Arrange
+    const handleLogin = jest.fn();
+    const handleSignUp = jest.fn();
+    const loginUserData = {
+      email: 'test@email.com',
+      password: 'password123'
+    };
+    (axios.post as jest.Mock).mockResolvedValue({ data: { resultCode: 200 } });
 
-  const emailInput = getByPlaceholderText('Email');
-  fireEvent.change(emailInput, { target: { value: 'test@example.com', name: 'email' } });
-  expect(emailInput.getAttribute('value')).toBe('test@example.com');
+    // Act
+    const { getByPlaceholderText, getByText } = render(
+      <Login handleLogin={handleLogin} handleSignUp={handleSignUp} />
+    );
+    const emailInput = getByPlaceholderText('Email');
+    const passwordInput = getByPlaceholderText('Password');
+    const submitButton = getByText('Log In');
 
-  const passwordInput = getByPlaceholderText('Password');
-  fireEvent.change(passwordInput, { target: { value: 'password', name: 'password' } });
-  expect(passwordInput.getAttribute('value')).toBe('password');
-});
+    fireEvent.change(emailInput, { target: { value: loginUserData.email } });
+    fireEvent.change(passwordInput, { target: { value: loginUserData.password } });
+    fireEvent.click(submitButton);
 
-it('Displays an error message when email or password is missing', () => {
-  const handleLogin = jest.fn();
-  const handleSignUp = jest.fn();
-  const { getByPlaceholderText, getByText } = render(<Login handleLogin={handleLogin} handleSignUp={handleSignUp} />);
-
-  const emailInput = getByPlaceholderText('Email');
-  fireEvent.change(emailInput, { target: { value: '', name: 'email' } });
-
-  const passwordInput = getByPlaceholderText('Password');
-  fireEvent.change(passwordInput, { target: { value: '', name: 'password' } });
-
-  const submitButton = getByText('Log In');
-  fireEvent.click(submitButton);
-
-  const errorMessage = getByText('All fields are required');
-  expect(errorMessage).toBeDefined();
+    // Assert
+    await waitFor(() => {
+      expect(axios.post).toHaveBeenCalledWith('/api/admin/login', loginUserData);
+    });
+  });
 });
