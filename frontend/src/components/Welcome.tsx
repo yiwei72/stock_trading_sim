6a5ac58900d8c129f5de407b1998973152910c2c
Felix Jing <32583955+fjing1@users.noreply.github.com>
2023-02-06 14:06:13 -0500

    updated the welcome.tsx from boyuan's, now showing first name,last name, etc, instead of empty page as before

--- frontend/src/components/Welcome.tsx
+++ frontend/src/components/Welcome.tsx
@@ -1,119 +1,81 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
 
 interface Props {
   handleLogout: () => void;
 }
-
 interface User {
   firstName: string;
   lastName: string;
   balance: number;
 }
-
 interface Stock {
   symbol: string;
   averagePrice: number;
   quantity: number;
   lastUpdated: string;
 }
-
 const Welcome: React.FC<Props> = ({ handleLogout }) => {
-  const sampleUser: User = {
-    firstName: "John",
-    lastName: "Doe",
-    balance: 1000
-  };
-  const samplestock: Stock = {
-    symbol: "a",
-    averagePrice: 1,
-    quantity: 10,
-    lastUpdated: "string"
-  };
-  const stock1: Stock[] = [samplestock];
-  
-  const [user, setUser] = useState<User>({...sampleUser});
-  const [stocks, setStocks] = useState<Stock[]>([...stock1]);
+  const [user, setUser] = useState<User | null>(null);
+  const [stocks, setStocks] = useState<Stock[]>([]);
 
-  
-  setUser({...sampleUser}); 
-  // const stock1: Stock[] = [samplestock];
-  setStocks([...stock1]);
-  
-  // useEffect(() => {
-  //   // Fetch user data from the backend API
-  //   fetch("https://api.example.com/user")
-  //     .then(response => response.json())
-  //     .then(data => setUser(data));
+  useEffect(() => {
+    // Fetch user data from the backend API
+    fetch("https://api.example.com/user")
+      .then(response => response.json())
+      .then(data => setUser(data));
 
-  //   // Fetch stock data from the backend API
-  //   fetch("https://api.example.com/stocks")
-  //     .then(response => response.json())
-  //     .then(data => setStocks(data));
-  // }, []);
-
+    // Fetch stock data from the backend API
+    fetch("https://api.example.com/stocks")
+      .then(response => response.json())
+      .then(data => setStocks(data));
+  }, []);
   return (
-    // <h1>what is gongig on</h1>
     <div>
-    {/* User Information Table */}
-    <table>
-      <thead>
-        <tr>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Balance</th>
-        </tr>
-      </thead>
-      <tbody>
-        {user && (
+      {/* User Information Table */}
+      <table>
+        <thead>
           <tr>
-            <td>{user.firstName}</td>
-            <td>{user.lastName}</td>
-            <td>{user.balance}</td>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Balance</th>
           </tr>
-        )}
-      </tbody>
-    </table>
-
-    {/* Stock Information Table */}
-    <table>
-      <thead>
-        <tr>
-          <th>Stock Symbol</th>
-          <th>Average Price</th>
-          <th>Quantity</th>
-          <th>Last Updated</th>
-        </tr>
-      </thead>
-      <tbody>
-        {stocks.map(stock => (
-          <tr key={stock.symbol}>
-            <td>{stock.symbol}</td>
-            <td>{stock.averagePrice}</td>
-            <td>{stock.quantity}</td>
-            <td>{stock.lastUpdated}</td>
+        </thead>
+        <tbody>
+          {user && (
+            <tr>
+              <td>{user.firstName}</td>
+              <td>{user.lastName}</td>
+              <td>{user.balance}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+      {/* Stock Information Table */}
+      <table>
+        <thead>
+          <tr>
+            <th>Stock Symbol</th>
+            <th>Average Price</th>
+            <th>Quantity</th>
+            <th>Last Updated</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
-    <div>
-       <button onClick={handleLogout}>Logout</button>
-     </div>
-  </div>
-    
-    
-    
-  
+        </thead>
+        <tbody>
+          {stocks.map(stock => (
+            <tr key={stock.symbol}>
+              <td>{stock.symbol}</td>
+              <td>{stock.averagePrice}</td>
+              <td>{stock.quantity}</td>
+              <td>{stock.lastUpdated}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div>
+        <button onClick={handleLogout}>Logout</button>
+      </div>
+    </div>
   );
 };
-
-// const Welcome: React.FC<Props> = ({ handleLogout }) => {
-//   return (
-//     <div>
-//       <h1>Welcome</h1>
-//       <button onClick={handleLogout}>Logout</button>
-//     </div>
-//   );
-// };
 
 export default Welcome;
