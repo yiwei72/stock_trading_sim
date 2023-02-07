e2baf6d202adc60857b05e6d8cacc10b33bbf54f
Felix Jing <32583955+fjing1@users.noreply.github.com>
2023-02-07 14:52:29 -0500

    adjusted the api data flow logic

--- frontend/src/components/Api.tsx
+++ frontend/src/components/Api.tsx
@@ -5,7 +5,8 @@ export async function getStockPrice(symbol: string) {
   const res = await axios.get(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=7U6WYWYO97NRET1Y`);
   //const {symbol: stockSymbol, open, high, low, price, volume, latestTradingDay, previousClose, change, changePercent} = res.data['Global Quote'];
   //console.log(res.data);
-  console.log(res.data["Global Quote"]["05. price"])
+  return res.data["Global Quote"]["05. price"]
+  
   //console.log(price);
 }
 
@@ -13,7 +14,8 @@ export async function getStockPrice(symbol: string) {
 To resolve this issue, you should wrap the getStockPrice function in another function that calls it and passes the event as an argument to getStockPrice.
 */
 export async function fetchStockPrice(symbol: string) {
-  const stockPrice = await getStockPrice(symbol).catch(console.error);;
+  const stockPrice = await getStockPrice(symbol).catch(console.error);
+  console.log(stockPrice)
 }
 //tsc alphavantageapi.ts to generate .js file
 //node alphavantageapi.js to test this file function, should print out the stock most recent data with high low, price, volume, etc
