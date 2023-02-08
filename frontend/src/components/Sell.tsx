1fc64103d72ee738b57f3040cc40a6c22217d819
Qinyi Xu <53362789+qinyi-xu@users.noreply.github.com>
2023-02-08 14:18:18 -0500

    Buy and sell page refresh button display (#30)
    * Buy and sell page refresh button display
    
    * Fix unused function build failure
    
    * Fix unused function build failure

--- frontend/src/components/Sell.tsx
+++ frontend/src/components/Sell.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+// import axios from "axios";
 import { fetchStockPrice } from "./Api";
 interface Props {
   handleLogin: () => void;
@@ -9,10 +9,10 @@ interface User {
   lastName: string;
   balance: number;
 }
-interface Stock {
-  val: number;
-  lastUpdated: string;
-}
+// interface Stock {
+//   val: number;
+//   lastUpdated: string;
+// }
 const Buy: React.FC<Props> = ({ handleLogin }) => {
   const [UserData, setUserData] = useState<User>({
     firstName: "",
@@ -22,87 +22,89 @@ const Buy: React.FC<Props> = ({ handleLogin }) => {
 
   const [isLoading, setIsLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
-  const [stocksymbol, setstocksymbol] = useState("");
-  const [stockval, setstockval] = useState<Stock>({ val: 0, lastUpdated: "" });
+  const [stockSymbol, setStockSymbol] = useState("");
+  const [stockVal, setStockVal] = useState(0);
+  const [lastUpdateTime, setLastUpdateTime] = useState("");
+  const [amount, setAmount] = useState(0);
   useEffect(() => {
     // Fetch user data from the backend API
     fetch("https://api.example.com/user")
       .then((response) => response.json())
       .then((data) => setUserData(data));
   }, []);
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { value } = event.target;
-    setstocksymbol(value);
-  };
+  // const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  //   const { value } = event.target;
+  //   setstocksymbol(value);
+  // };
 
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+  // const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+  //   event.preventDefault();
+  //   setIsLoading(true);
+
+  //   try {
+  //     //maybe some input restriction
+  //     const response = await axios.post("/api/admin/login", stockSymbol);
+  //     console.log(response.data);
+  //     //some handle
+  //     setStockVal(response.data);
+  //     if (response.data.resultCode === 200) {
+  //       //get data here
+  //     } else {
+  //       setErrorMessage(response.data.data);
+  //     }
+  //   } catch (error: any) {
+  //     setErrorMessage(error.message);
+  //   } finally {
+  //     setIsLoading(false);
+  //   }
+  // };
+  async function handleClickPrice(event: React.MouseEvent<HTMLButtonElement>) {
     event.preventDefault();
     setIsLoading(true);
-
     try {
-      //maybe some input restriction
-      const response = await axios.post("/api/admin/login", stocksymbol);
-      console.log(response.data);
-      //some handle
-      setstockval(response.data);
-      if (response.data.resultCode === 200) {
-        //get data here
-      } else {
-        setErrorMessage(response.data.data);
+      if (!stockSymbol) {
+        throw new Error("Stock Symbol is required");
       }
+      setStockVal(await fetchStockPrice(stockSymbol).catch(console.error));
+      setLastUpdateTime(new Date().toLocaleString());
     } catch (error: any) {
       setErrorMessage(error.message);
     } finally {
       setIsLoading(false);
     }
-  };
-  function handleClickPrice(event: React.MouseEvent<HTMLButtonElement>) {
-    event.preventDefault();
-    fetchStockPrice("AAPL").catch(console.error);
   }
+
   return (
     <div>
       <p>First Name: {UserData.firstName}</p>
       <p>Last Name: {UserData.lastName}</p>
       <p>Balance: {UserData.balance}</p>
       <div>
-        <form onSubmit={handleSubmit}>
-          <label>Stock Symbol:</label>
-          <input
-            type="text"
-            name="stocksymbol"
-            value={stocksymbol}
-            onChange={handleInputChange}
-          />
-        </form>
+        <label>Stock Symbol:</label>
+        <input
+          type="text"
+          name="stockSymbol"
+          value={stockSymbol}
+          onChange={(e) => setStockSymbol(e.target.value)}
+        />
         {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
         {!errorMessage && (
           <div>
-            <p>stock value:{stockval.val} </p>
-            <p>stock holding amount:</p>
-            <p>lasupdated:{stockval.lastUpdated}</p>
+            <p>stock value:{stockVal} </p>
+            <p>last updated:{lastUpdateTime}</p>
           </div>
         )}
         <button type="submit" disabled={isLoading} onClick={handleClickPrice}>
           {isLoading ? "Refreshing" : "Refresh"}
         </button>
-        <form onSubmit={handleSubmit}>
-          <label>Stock Symbol:</label>
-          <input
-            type="text"
-            name="stocksymbol"
-            value={stocksymbol}
-            onChange={handleInputChange}
-          />
-          <label>Sell Amount:</label>
-          <input
-            type="text"
-            name="Amounut"
-            value={stocksymbol}
-            onChange={handleInputChange}
-          />
-        </form>
-
+        <br></br>
+        <label>Buy Amount:</label>
+        <input
+          type="number"
+          name="amount"
+          value={amount}
+          onChange={(e) => setAmount(Number(e.target.value))}
+        />
         <button type="submit" disabled={isLoading}>
           {isLoading ? "Selling" : "Sell"}
         </button>
