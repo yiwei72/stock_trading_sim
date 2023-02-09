f4d654fcf1eebc2ff9c292e9d2ceadeed0bc1233
frontend code format <code@format>
2023-02-09 04:20:49 +0000

    format with prettier

--- frontend/src/tests/Api.test.tsx
+++ frontend/src/tests/Api.test.tsx
@@ -1,34 +1,34 @@
-import axios from 'axios';
-import { fetchStockPrice } from '../components/Api';
+import axios from "axios";
+import { fetchStockPrice } from "../components/Api";
 
-jest.mock('axios');
+jest.mock("axios");
 
-describe('fetchStockPrice', () => {
-  it('fetches stock price from the API', async () => {
+describe("fetchStockPrice", () => {
+  it("fetches stock price from the API", async () => {
     (axios.get as jest.Mocked<any>).mockResolvedValue({
       data: {
-        'Global Quote': {
-          '05. price': 150.00
-        }
-      }
+        "Global Quote": {
+          "05. price": 150.0,
+        },
+      },
     });
 
-    const stockPrice = await fetchStockPrice('AAPL');
+    const stockPrice = await fetchStockPrice("AAPL");
     //testing range
     expect(stockPrice).toBeGreaterThanOrEqual(100);
     expect(stockPrice).toBeLessThanOrEqual(200);
     expect(axios.get).toHaveBeenCalledWith(
-      'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=AAPL&apikey=7U6WYWYO97NRET1Y'
+      "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=AAPL&apikey=7U6WYWYO97NRET1Y"
     );
   });
 
-  it('handles API errors', async () => {
-    (axios.get as jest.Mocked<any>).mockRejectedValue(new Error('API Error'));
+  it("handles API errors", async () => {
+    (axios.get as jest.Mocked<any>).mockRejectedValue(new Error("API Error"));
 
     try {
-      await fetchStockPrice('AAPL');
-    } catch (error:any) {
-      expect(error.message).toBe('API Error');
+      await fetchStockPrice("AAPL");
+    } catch (error: any) {
+      expect(error.message).toBe("API Error");
     }
   });
 });
