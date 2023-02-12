1739455d417cfaa1e03fbe032dba3c8908a46962
Yiwei Wang <yiwei.wang@uwaterloo.ca>
2023-02-12 17:55:39 -0500

    add test to buy and sell button

--- frontend/src/tests/Welcome.test.tsx
+++ frontend/src/tests/Welcome.test.tsx
@@ -13,9 +13,25 @@ jest.mock("axios", () => ({
 jest.mock("react-router-dom", () => {
   return {
     ...jest.requireActual("react-router-dom"),
-    useNavigate: jest.fn(),
+    useNavigate: jest.fn().mockReturnValue(jest.fn()),
   };
 });
+
+const user = {
+  firstName: "father",
+  lastName: "admin",
+  balance: 100000000,
+  holding: [
+    {
+      assetNumber: 1,
+      email: "admin@uwaterloo.ca",
+      stockSymbol: "AAPL",
+      price: 200,
+      quantity: 5,
+      timeStamp: 1494722022853,
+    },
+  ],
+};
 
 describe("Test Welcome component", () => {
   it("click the Logout button and it goes to /login", async () => {
@@ -31,17 +47,9 @@ describe("Test Welcome component", () => {
     expect(navigate).toHaveBeenCalledWith("/login");
   });
 
-
   it('updates the first name', async () => {
     (axios.post as jest.Mocked<any>).mockResolvedValue({
-      data: {
-        data: {
-          firstName: "John",
-          lastName: "Doe",
-          balance: 666,
-          holding: []
-        },
-      },
+      data: { data: user },
     });
 
     const updateFirstName = jest.fn()
@@ -60,5 +68,58 @@ describe("Test Welcome component", () => {
     });
 
     expect(updateFirstName).toHaveBeenCalled();
+  });
+
+  it("should navigate to /buy when Buy button is clicked", async () => {
+    const navigate = jest.fn();
+    (useNavigate as jest.Mock).mockReturnValue(navigate);
+
+    (axios.post as jest.Mock).mockResolvedValue({
+      data: { data: user },
+    });
+
+    const component = (
+      <MemoryRouter>
+        <EmailContext.Provider value={{ email: '', updateEmail: jest.fn() }}>
+          <UserAuthContext.Provider value={{ firstName: '', updateFirstName: jest.fn() }}>
+            <Welcome />
+          </UserAuthContext.Provider>
+        </EmailContext.Provider>
+      </MemoryRouter>
+    );
+
+    await act(async () => {
+      const { getByText } = render(component);
+      fireEvent.click(getByText("Buy"));
+      await new Promise((resolve) => setTimeout(resolve, 0));
+      expect(navigate).toHaveBeenCalledWith("/buy", { state: { user: {balance: 0, firstName: "", lastName: "", holding: []} } });
+    });
+  });
+
+  it("should navigate to /sell when Sell button is clicked", async () => {
+    const navigate = jest.fn();
+    (useNavigate as jest.Mock).mockReturnValue(navigate);
+
+
+    (axios.post as jest.Mock).mockResolvedValue({
+      data: { data: user },
+    });
+
+    const component = (
+      <MemoryRouter>
+        <EmailContext.Provider value={{ email: '', updateEmail: jest.fn() }}>
+          <UserAuthContext.Provider value={{ firstName: '', updateFirstName: jest.fn() }}>
+            <Welcome />
+          </UserAuthContext.Provider>
+        </EmailContext.Provider>
+      </MemoryRouter>
+    );
+
+    await act(async () => {
+      const { getByText } = render(component);
+      fireEvent.click(getByText("Sell"));
+      await new Promise((resolve) => setTimeout(resolve, 0));
+      expect(navigate).toHaveBeenCalledWith("/sell", { state: { user: {balance: 0, firstName: "", lastName: "", holding: []} } });
+    });
   });
 });
