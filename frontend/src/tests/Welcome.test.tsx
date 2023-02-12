a11a6ac736c69066f825ad20a1647b728cb036ea
Yiwei Wang <yiwei.wang@uwaterloo.ca>
2023-02-12 17:21:12 -0500

    add coverage for line 41 at Welcome

--- frontend/src/tests/Welcome.test.tsx
+++ frontend/src/tests/Welcome.test.tsx
@@ -1,10 +1,10 @@
 import axios from "axios";
 import React from "react";
 import { render, fireEvent, waitFor, act } from "@testing-library/react";
-import { BrowserRouter as Router, useNavigate } from "react-router-dom";
-import Login from "../components/Login";
+import { BrowserRouter, Router, useNavigate, MemoryRouter } from "react-router-dom";
 import Welcome from "../components/Welcome";
-import Signup from "../components/Signup";
+import { EmailContext, UserAuthContext } from "../Context";
+
 jest.mock("axios", () => ({
   get: jest.fn(() => Promise.resolve({ data: {} })),
   post: jest.fn(() => Promise.resolve({ data: {} })),
@@ -16,62 +16,49 @@ jest.mock("react-router-dom", () => {
     useNavigate: jest.fn(),
   };
 });
+
 describe("Test Welcome component", () => {
-  /*
-  it("click the Buy button and it goes to /buy", async () => {
-    const user = {
-      firstName: "father",
-      lastName: "admin",
-      balance: 	100000000,
-      holding:  [{
-        assetNumber: 1,
-        email: "admin@uwaterloo.ca",
-        stockSymbol: "AMZN",
-        price: 105.15,
-        quantity: 300,
-        timeStamp: 1494722022853,
-      },{
-        assetNumber: 1,
-        email: "admin@uwaterloo.ca",
-        stockSymbol: "META",
-        price: 153.12,
-        quantity: 100,
-        timeStamp: 1494722022853,
-      },{
-        assetNumber: 1,
-        email: "admin@uwaterloo.ca",
-        stockSymbol: "MSFT",
-        price: 252.75,
-        quantity: 200,
-        timeStamp: 1494712022853,
-      },
-    ]
-  };
-    const navigate = jest.fn();
-    (useNavigate as jest.Mock).mockReturnValue(navigate);
-    const {getByText } = render(
-      <Router>
-        <Welcome />
-      </Router>
-    );
-    const BuyButton = getByText("Buy");
-    fireEvent.click(BuyButton);
-    await new Promise((resolve) => setTimeout(resolve, 0));
-    expect(navigate).toHaveBeenCalledWith("/buy");
-    //expect(history.location.state).toEqual({ user: user });
-  });
-  */
   it("click the Logout button and it goes to /login", async () => {
     const navigate = jest.fn();
     (useNavigate as jest.Mock).mockReturnValue(navigate);
     const { getByPlaceholderText, getByText } = render(
-      <Router>
+      <BrowserRouter>
         <Welcome />
-      </Router>
+      </BrowserRouter>
     );
     const SellButton = getByText("Logout");
     fireEvent.click(SellButton);
-    // await new Promise((resolve) => setTimeout(resolve, 0));
     expect(navigate).toHaveBeenCalledWith("/login");
+  });
+
+
+  it('updates the first name', async () => {
+    (axios.post as jest.Mocked<any>).mockResolvedValue({
+      data: {
+        data: {
+          firstName: "John",
+          lastName: "Doe",
+          balance: 666,
+          holding: []
+        },
+      },
+    });
+
+    const updateFirstName = jest.fn()
+    const component = (
+      <MemoryRouter>
+        <EmailContext.Provider value={{ email: '', updateEmail: jest.fn() }}>
+          <UserAuthContext.Provider value={{ firstName: '', updateFirstName }}>
+            <Welcome />
+          </UserAuthContext.Provider>
+        </EmailContext.Provider>
+      </MemoryRouter>
+    );
+
+    await act(async () => {
+      render(component);
+    });
+
+    expect(updateFirstName).toHaveBeenCalled();
   });
 });
