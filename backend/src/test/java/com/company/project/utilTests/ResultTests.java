ea5161f5cceee6fb76d1dae6c056d7aa6c29c79b
Felix Jing <32583955+fjing1@users.noreply.github.com>
2023-02-12 21:18:14 -0500

    renamed the ResultTests.java

--- backend/src/test/java/com/company/project/utilTests/ResultTests.java
+++ backend/src/test/java/com/company/project/utilTests/ResultTests.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 //import static org.junit.jupiter.api.Assertions.assertFalse;
 
-public class ResultTest {
+public class ResultTests {
     @Test
     public void testResultConstructorWithIntAndString() {
         int expectedCode = 200;
