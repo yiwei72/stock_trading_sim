753b84a3bf94f84ec75017bb0c42a923ba89d889
Felix Jing <32583955+fjing1@users.noreply.github.com>
2023-02-17 23:51:56 -0500

    PriceCalculator 100%

--- backend/src/test/java/com/company/project/utilTests/PriceCalculatorTests.java
+++ backend/src/test/java/com/company/project/utilTests/PriceCalculatorTests.java
@@ -1,21 +1,32 @@
 package com.company.project.utilTests;
-
 import com.company.project.util.PriceCalculator;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
 import java.math.RoundingMode;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.junit.jupiter.api.BeforeEach;
 
 @SpringBootTest
 public class PriceCalculatorTests {
 
+    @Mock
+    private PriceCalculator priceCalculator1;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+        priceCalculator1 = new PriceCalculator();
+    }
     @Test
     public void testUpdateAvePrice() {
         Double result =
                 PriceCalculator.updateAvePrice(100.0, 100L, 50.0, 50L, 1, 4, RoundingMode.HALF_UP);
         assertEquals("83.3333", result.toString());
+        Double result2 =
+            PriceCalculator.updateAvePrice(100.0, 2L, 50.0, 1L, -1, 4, RoundingMode.HALF_UP);
+        assertEquals(150, result2, 0.0001);
     }
 
     @Test
