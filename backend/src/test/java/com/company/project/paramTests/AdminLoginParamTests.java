969f44604a7d7fb21477d7aea9c8ea1b629e9f51
Felix Jing <32583955+fjing1@users.noreply.github.com>
2023-02-25 23:17:12 -0500

    AdminLoginParamTests covers 89% instrction  and 59 branch%

--- backend/src/test/java/com/company/project/paramTests/AdminLoginParamTests.java
+++ backend/src/test/java/com/company/project/paramTests/AdminLoginParamTests.java
@@ -4,17 +4,14 @@ package com.company.project.paramTests;
 import com.company.project.controllers.param.AdminLoginParam;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import java.math.RoundingMode;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import org.junit.jupiter.api.BeforeEach;
-import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 //for testing case
 import javax.validation.ConstraintViolation;
@@ -32,50 +29,15 @@ public class AdminLoginParamTests {
         ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
         validator = factory.getValidator();
     }
-    @Test
-    public void testAdminLoginParam() {
-        // Setup
-        String email = "test@example.com";
-        String password = "password123";
-        AdminLoginParam adminLoginParam = new AdminLoginParam();
-        adminLoginParam.setEmail(email);
-        adminLoginParam.setPassword(password);
-        // Verify
-        assertEquals(email, adminLoginParam.getEmail());
-        assertEquals(password, adminLoginParam.getPassword());
-    }
+
     @Test
     void testAdminLoginParamValidInput() {
         // Setup
         AdminLoginParam adminLoginParam = new AdminLoginParam();
-        adminLoginParam.setEmail("test@example.com");
-        adminLoginParam.setPassword("password");
+        adminLoginParam.setEmail("admin@uwaterloo.ca");
+        adminLoginParam.setPassword("123456");
         // Verify
         Set<ConstraintViolation<AdminLoginParam>> violations = validator.validate(adminLoginParam);
-        assertTrue(violations.isEmpty());
-    }
-    @Test
-    void testAdminLoginParamEmptyFields() {
-        AdminLoginParam param = new AdminLoginParam();
-        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
-        Validator validator = factory.getValidator();
-        Set<ConstraintViolation<AdminLoginParam>> violations = validator.validate(param);
-
-        assertEquals(2, violations.size());
-        for (ConstraintViolation<AdminLoginParam> violation : violations) {
-            String message = violation.getMessage();
-            assertTrue(message.contains("can't be empty"));
-        }
-    }
-    @Test
-    void testAdminLoginParamValidFields() {
-        AdminLoginParam param = new AdminLoginParam();
-        param.setEmail("test@example.com");
-        param.setPassword("password");
-        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
-        Validator validator = factory.getValidator();
-        Set<ConstraintViolation<AdminLoginParam>> violations = validator.validate(param);
-
         assertTrue(violations.isEmpty());
     }
     @Test
@@ -105,46 +67,73 @@ public class AdminLoginParamTests {
         param3.setPassword("password");
         assertEquals(param1.hashCode(), param2.hashCode());
         assertNotEquals(param1.hashCode(), param3.hashCode());
+        assertNotEquals(param2.hashCode(), param3.hashCode());
     }
     @Test
     void testAdminLoginParamToString() {
         AdminLoginParam param = new AdminLoginParam();
         param.setEmail("test@example.com");
         param.setPassword("password");
-
         String expected = "AdminLoginParam(email=test@example.com, password=password)";
         assertEquals(expected, param.toString());
+        assertNotEquals("haha", param.toString());
     }
-//for @Data  hashcode() and equals(objects)
-@Test
-public void testData() {
-    AdminLoginParam param1 = new AdminLoginParam();
-    param1.setEmail("test@example.com");
-    param1.setPassword("password1");
+    @Test
+    public void testEquals() {
+        // Scenario 1: both instances have the same email and password values
+        // Arrange
+        AdminLoginParam param1 = new AdminLoginParam();
+        param1.setEmail("test@example.com");
+        param1.setPassword("password");
+        AdminLoginParam param2 = new AdminLoginParam();
+        param2.setEmail("test@example.com");
+        param2.setPassword("password");
+        // Act
+        boolean result = param1.equals(param2);
 
-    AdminLoginParam param2 = new AdminLoginParam();
-    param2.setEmail("test@example.com");
-    param2.setPassword("password1");
+        // Assert
+        assertTrue(result);
 
-    AdminLoginParam param3 = new AdminLoginParam();
-    param3.setEmail("test2@example.com");
-    param3.setPassword("password2");
+        // Scenario 3: both instances have different password values
+        // Arrange
+        param1.setEmail("test@example.com");
+        param1.setPassword("password1");
+        param2.setEmail("test@example.com");
+        param2.setPassword("password2");
+        // Act
+        result = param1.equals(param2);
 
-    // test equals()
-    assertTrue(param1.equals(param2));
-    assertFalse(param1.equals(param3));
-
-    // test hashCode()
-    assertEquals(param1.hashCode(), param2.hashCode());
-    assertNotEquals(param1.hashCode(), param3.hashCode());
-
-    // test toString()
-    assertEquals("AdminLoginParam(email=test@example.com, password=password1)", param1.toString());
+        // Assert
+        assertFalse(result);
+        // Scenario 4: both instances have different email but same password values
+        // Arrange
+        param1.setEmail("test1@example.com");
+        param1.setPassword("same");
+        param2.setEmail("test2@example.com");
+        param2.setPassword("same");
+        // Act
+        result = param1.equals(param2);
+        // Assert
+        assertFalse(result);
+        // Scenario 5: comparing an instance to itself and comparing to null
+        // Arrange
+        param1.setEmail("test@example.com");
+        param1.setPassword("password");
+        // Act
+        result = param1.equals(param1);
+        boolean nullResult = param1.equals(null);
+        // Assert
+        assertTrue(result);
+        assertFalse(nullResult);
+        // Scenario 6: comparing 2 null
+        param1.setEmail("");
+        param1.setPassword("");
+        param2.setEmail("");
+        param2.setPassword("");
+        // Act
+        result = param1.equals(param2);
+        assertTrue(result);
+    }
 }
 
-
-
-
-
-}
 
