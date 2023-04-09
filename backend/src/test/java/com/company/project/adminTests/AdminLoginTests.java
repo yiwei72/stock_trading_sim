132c830542af8165ce0eee4ac781c75a5093fffa
Cuiyang Wang <113805046+cuiyangwang@users.noreply.github.com>
2023-04-08 22:37:29 -0400

    fix AdminLoginTests.testSuccessfulLogin bug (#75)

--- backend/src/test/java/com/company/project/adminTests/AdminLoginTests.java
+++ backend/src/test/java/com/company/project/adminTests/AdminLoginTests.java
@@ -17,7 +17,7 @@ public class AdminLoginTests {
     @Test
     void testSuccessfulLogin() {
         String email = "admin@uwaterloo.ca";
-        String password = "123456";
+        String password = "8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92";
         AdminLoginParam adminLoginParam = new AdminLoginParam();
         adminLoginParam.setEmail(email);
         adminLoginParam.setPassword(password);
